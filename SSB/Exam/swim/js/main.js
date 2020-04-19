f6f5f14e5bcd478f38b6e22c2c3e4fb41948db92
Saiful Islam Shanto <52715000+Helloshanto@users.noreply.github.com>
2020-04-19 18:53:43 +0600

    Add files via upload

---
+++ SSB/Exam/swim/js/main.js
@@ -0,0 +1,33 @@
+$('.owl-carousel').owlCarousel({
+    margin: 30,
+    loop: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 2
+        }
+    }
+});
+
+
+
+$('#testimonial-carousel').owlCarousel({
+    loop: true,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
