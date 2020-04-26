af9313cca206d835eb026f0733280d27f14bc274
Saiful Islam Shanto <52715000+Helloshanto@users.noreply.github.com>
2020-04-26 21:45:10 +0600

    Add files via upload

--- Self-Practice/TryDo-protfolio/js/main.js
+++ Self-Practice/TryDo-protfolio/js/main.js
@@ -1,24 +1,71 @@
 (function ($) {
-    'use strict';
+  'use strict';
 
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 30,
-        center: true,
-        nav: false,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 2
-            },
-            1000: {
-                items: 3
-            }
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    center: true,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
+
+
+  $('.test-inner').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
         }
-    });
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 
+  $('.logos-slider').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+
+  
 
 
 })(jQuery);
